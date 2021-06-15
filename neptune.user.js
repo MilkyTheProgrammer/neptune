dbb1d30eef2a7403db4cc7a79c1c5e9abfee454a
PhoenixTheCoder <75660894+PhoenixTheCoder@users.noreply.github.com>
2021-06-15 13:08:37 -0700

    Update neptune.user.js

--- neptune.user.js
+++ neptune.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Neptune
+// @name         Neptune v3
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  A really amazing MPP midi script
