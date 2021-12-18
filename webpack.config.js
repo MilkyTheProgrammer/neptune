438b81b705036586fb0f69b9fd94291bb3cf73a4
Hri7566 <hri7566@gmail.com>
2021-12-18 04:20:56 -0500

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// webpack configuration file
+
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'neptune.user.js'
+    }
+}
